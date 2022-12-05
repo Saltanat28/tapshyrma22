b5c179208326ace59a5b027374150e3ac26aaf7d
saltanat <saltanatnematillakyzy6@gmail.com>
2022-12-05 18:07:39 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Random random = new Random();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            numbers.add(random.nextInt(1,100));
+        }
+        System.out.println(numbers);
+        System.out.println(numbers.size());
+
+
+        System.out.println("_____Even numbers_____");
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (number%2==0){
+                evenNumbers.add(number);
+            }
+        }
+        for (Integer evenNumber : evenNumbers) {
+            System.out.print(evenNumber  +" ");
+        }
+
+
+        System.out.println();
+        System.out.println("____Odd numbers_____");
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        for (Integer number1: numbers) {
+            if(number1%2==1){
+                oddNumbers.add(number1);
+            }
+        }
+        for (Integer oddNumber: oddNumbers) {
+
+            System.out.print(oddNumber + " ");
+        }
     }
 }
